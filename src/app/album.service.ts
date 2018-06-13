f2f0e2ff051ff76a54ab595f23763fa6b43f0cab
Azamat Bekmuratov and andrew lupton <andrew@>
2018-06-13 10:58:08 -0700

    modify album detail component

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -30,4 +30,8 @@ export class AlbumService {
                                 description: localUpdatedAlbum.description});
   }
 
+  deleteAlbum(localAlbumToDelete){
+    var albumEntryInFirebase = this.getAlbumById(localAlbumToDelete.$key);
+    albumEntryInFirebase.remove();
+  }
 }
